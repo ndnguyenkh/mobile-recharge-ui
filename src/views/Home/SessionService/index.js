bfa9b0b491e4a7ff086f265d528cac5e6878be23
ndnguyenkh <nguyenduynguyenkh@gmail.com>
2024-12-17 12:57:03 +0700

    commit logger

--- src/views/Home/SessionService/index.js
+++ src/views/Home/SessionService/index.js
@@ -1,10 +1,44 @@
 
-import { Box, Grid, Link, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, Grid, Link, Typography } from "@mui/material";
+import { toast } from "react-toastify";
 import MoneyIcon from '@mui/icons-material/Money';
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import ExtensionIcon from '@mui/icons-material/Extension';
 
+import { useAuth } from "~/config/AuthProvider";
+import { ErrorCodes } from "~/utils/Common/Message";
+
+
 const SessionService = () => {
+
+    const { auth } = useAuth();
+    const navigate = useNavigate();
+
+    const handleOpenAuthLoggedBill = () => {
+        try {
+            if (!auth) {
+                navigate("/login");
+            } else {
+                navigate("/pay-the-bills");
+            }
+        } catch (error) {
+            toast.error(ErrorCodes.SERVER_ERROR.message);
+        }
+    };
+
+    const handleOpenAuthLoggedGame = () => {
+        try {
+            if (!auth) {
+                navigate("/login");
+            } else {
+                navigate("/spinning");
+            }
+        } catch (error) {
+            toast.error(ErrorCodes.SERVER_ERROR.message);
+        }
+    };
+
     return ( 
         <Box sx={{my: '50px', mx: '150px'}}>
             <Typography variant="h4" sx={{textAlign: 'center', fontWeight: 'bold', color: 'gray', my: 8}}>Services</Typography>
@@ -21,7 +55,9 @@ const SessionService = () => {
                     <Grid item md={4}>
                         <Box sx={{height: '200px', border: '1px solid pink', borderRadius: '10px'}}>
                             <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                                <Link href="/pay-the-bills"><ReceiptLongIcon sx={{width: '150px', height: '150px', color: 'pink', cursor: 'pointer', ":hover": {color: 'gray'}}} /></Link>
+                                <Button onClick={handleOpenAuthLoggedBill}>
+                                    <Link><ReceiptLongIcon sx={{width: '150px', height: '150px', color: 'pink', cursor: 'pointer', ":hover": {color: 'gray'}}} /></Link>
+                                </Button>
                             </Box>
                             <Typography variant="h5" sx={{textAlign: 'center', fontWeight: 'bold', color: 'gray'}}>Pay the bills</Typography>
                         </Box>
@@ -29,7 +65,9 @@ const SessionService = () => {
                     <Grid item md={4}>
                         <Box sx={{height: '200px', border: '1px solid pink', borderRadius: '10px'}}>
                             <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                            <Link href="/spinning"><ExtensionIcon sx={{width: '150px', height: '150px', color: 'pink', cursor: 'pointer', ":hover": {color: 'gray'}}} /></Link>
+                                <Button onClick={handleOpenAuthLoggedGame}>
+                                    <Link><ExtensionIcon sx={{width: '150px', height: '150px', color: 'pink', cursor: 'pointer', ":hover": {color: 'gray'}}} /></Link>
+                                </Button>
                             </Box>
                             <Typography variant="h5" sx={{textAlign: 'center', fontWeight: 'bold', color: 'gray'}}>Mini Games</Typography>
                         </Box>
