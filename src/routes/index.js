3a0856a7af4ba777d4db66b51964dee0f712405c
ndnguyenkh <nguyenduynguyenkh@gmail.com>
2024-12-18 22:51:15 +0700

    commit final

--- src/routes/index.js
+++ src/routes/index.js
@@ -33,7 +33,7 @@ const PublicRoutes = [
 
     // temp
     // { path: '/pay-the-bills', component: PayTheBills },
-     { path: '/profile', component: Profile },
+    // { path: '/profile', component: Profile },
 
     // temp admin
     // { path: '/admin-recharge-phone', component: AdminRechargePhone, layout: ManageLayout },
@@ -46,7 +46,7 @@ const PrivateRoutes = [
     // user
     { path: '/pay-the-bills', component: PayTheBills },
     { path: '/spinning', component: Spinning },
-    // { path: '/profile', component: Profile },
+    { path: '/profile', component: Profile },
 
     // admin
     { path: '/admin-recharge-phone', component: AdminRechargePhone, layout: ManageLayout },
